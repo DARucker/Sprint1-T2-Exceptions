7e2f1647b4238836279bb9f17303b0a6e16a12b7
Dario <dariomsn@yahoo.com.ar>
2022-11-15 15:39:54 -0300

    Commit

--- Sprint1_Tasca2/src/n1exercici1/Venta.java
+++ Sprint1_Tasca2/src/n1exercici1/Venta.java
@@ -8,8 +8,28 @@ public class Venta {
 
 	private List<Producto> productos = new ArrayList<>();
 	private double precioTotal;
-
+	public static Scanner teclado;
+	
 	Producto p = new Producto();
+
+	public void cargarDatosFactura() {
+
+		teclado = new Scanner(System.in);
+		
+		int continua = 1;
+		
+		while (continua  == 1) {
+
+			p.setNombre(capturarString());
+			p.setPrecio(capturarNumero("Ingrese precio: "));
+			productos.add(p);
+			
+			System.out.println("Presione 1 para ingresar otro producto: ");
+			int contimua = capturarNumero("presione 1 si desea cargar otro item");
+		}
+		
+		calcularTotal();
+	}
 
 	public void calcularTotal() {
 		
@@ -21,25 +41,29 @@ public class Venta {
 
 		System.out.println("Total: " + total);
 	}
+	
+	
+	public static String capturarString () {
+		String productoFactura;
+		System.out.println("Ingrese producto");
+		productoFactura = teclado.nextLine();
 
-	public void cargarDatosFactura() {
+		return productoFactura;
 
-		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Presione 1 para ingresar los productos a facturar: ");
-		int respuesta = sc.nextInt();
-
-		while (respuesta == 1) {
-			
-			
-			System.out.println("Ingrese producto");
-			p.setNombre(sc.nextLine());
-			System.out.println("Ingrese precio");
-			p.setPrecio(sc.nextDouble());
-			productos.add(p);
-			System.out.println("Presione 1 para ingresar otro producto: ");
-			respuesta = sc.nextInt();
-		}
-		calcularTotal();
 	}
+
+	public static int capturarNumero(String mensaje) {
+				
+		int datoNumerico = 0;
+		System.out.println(mensaje);
+		//return Integer.parseInt(teclado.nextLine());
+		datoNumerico = teclado.nextInt();
+		return datoNumerico;
+	
+	}
+
+
+
+
+
 }
